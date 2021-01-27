bf1726df3b714d813bf69fb5b5278aecf880d304
ninakvach <ninakvach@gmail.com>
2021-01-28 00:12:41 +0300

    1

---
+++ components/ReloadIcon.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import { View, Platform, StyleSheet } from 'react-native'
+import { Ionicons} from '@expo/vector-icons'
+import {colors} from '../utils/index'
+export default function ReloadIcon({load}) {
+    const reloadIconName = Platform.OS == 'ios' ? 'ios-refresh' : 'refresh'
+    return (
+        <View style={styles.reloadIcon}>
+           <Ionicons onPress={load} name={reloadIconName} size={24} color={colors.PRIMARY_COLOR} />
+        </View>
+    )
+}
+
+const styles = StyleSheet.create ({
+    reloadIcon:
+    {
+        ...Platform.select({
+            ios:{
+                top:-20
+            },
+            android:{
+                top:0
+            }
+            
+    }),
+        position: 'absolute',
+        top:20,
+        right:20,
+    }
+
+})
